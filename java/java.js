1a6e4315b5248eef250ca6a7b22010b246032aae
snitolya <maslop@bk.ru>
2022-05-11 21:17:52 +0300

    javascript

---
+++ java/java.js
@@ -0,0 +1,48 @@
+// let a = prompt("Пожалуйста введите число 1");
+// let b = prompt("Пожалуйста введите число 2");
+// a = Number(a);
+// b = Number(b);
+// let result = a + b;
+// alert (result);
+
+// let a = prompt("Пожалуйста введите число ");
+// a = Number(a);
+// let result = a **2;
+// alert(result);
+// result = a **10;
+// alert(result);
+
+// let a = prompt("Пожалуйста введите число ");
+// let b = prompt("Пожалуйста введите число ");
+// let c = prompt("Пожалуйста введите число 2");
+// a = Number(a);
+// b = Number(b);
+// c = Number(b);
+// let result =(a * 2) + (b - 3) + (c **2);
+// console.log("Рузультат", result, a*b);
+
+// let a = prompt("Пожалуйста введите число ");
+
+// a = Number(a);
+
+// let result =(a * 4);
+// console.log("площадь", result, a*4);
+// result = (a * a)
+// console.log("Рузультат", result, a*a);
+
+// let a = prompt("1 кг печенья");
+// let b = prompt("1 кг конфет ");
+// a = Number(a);
+// b = Number(b);
+// let result =( a / 1000 * 300 +  b / 1000 * 400 );
+// console.log("Общая стоимость", result,);
+
+
+let a = prompt("количество дней ");
+let b = prompt("процент скидки ");
+let b = prompt("сумма");
+ a = Number(a);
+ b = Number(b);
+ с = Number(с);
+let result = (a + b + c);
+console.log("Рузультат", result, a*b);
