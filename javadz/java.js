ba44657d115d7166b2a81cf8b01e6478f138aeea
nicksykozlov <nicksy.kozlov@gmail.com>
2022-05-18 15:42:55 +0300

    dzcykl

--- javadz/java.js
+++ javadz/java.js
@@ -1,5 +1,5 @@
 // alert( 'Я JavaScript!' );
-// alert('Привет'); 
+// alert('Привет');
 // alert('Мир');
 // alert('Hello')
 // [1, 2].forEach(alert);
@@ -94,7 +94,7 @@
 // let b = +prompt("Второе число?", 2);
 // alert(a + b); // 3
 
-// let a=prompt("введите сумму депозита"); // калькулятор 
+// let a=prompt("введите сумму депозита"); // калькулятор
 // let b=prompt("срок депозита в месяцах");
 // let result=a*(5/100)/365*30;
 // console.log("сумма процентов в месяц",result);
@@ -103,15 +103,14 @@
 // let itogViplota=summaZaVesSrok+result+(a++);
 // console.log('общая сумма к выплате в конце срока',itogViplota);
 
-<<<<<<< HEAD
-let number=prompt("введите число");
-number = (number%10) ;
-let numberB =  (''+number)[1];
-let numberC = 56 % 10;
-let numberG = 5 % 10;
+// let number=prompt("введите число");
+// number = (number%10) ;
+// let numberB =  (''+number)[1];
+// let numberC = 56 % 10;
+// let numberG = 5 % 10;
 
-alert (numberB);
-=======
+// alert (numberB);
+
 // let number=prompt("введите число");
 // number %= 10;
 // let numberB = 567 % 10;
@@ -120,7 +119,7 @@ alert (numberB);
 
 // alert (''+number  +numberB  +numberC  +numberG);
 
-// 16.05.2022 занятие 
+// 16.05.2022 занятие
 
 // блок Else
 // let year = prompt('В каком году была опубликована спецификация ECMAScript-2015?', '');
@@ -202,7 +201,7 @@ alert (numberB);
 //     alert(numberB)
 // }
 
-// задание 38 Доделать 
+// задание 38 Доделать
 
 // let numberA = +prompt ("ввидите число")
 // let numberB = +prompt ("Введите число")
@@ -211,14 +210,11 @@ alert (numberB);
 // if(numberC>100){
 //     alert('Yes')
 // }
-  
+
 // }
 // else {
 //     alert(numberB)
 // }
->>>>>>> 42d6118a338428dd9081eecaf28bd950c9c0851f
-
-
 
 // циклы
 
@@ -250,9 +246,117 @@ alert (numberB);
 // }
 // console.log(s);
 
-let a = +prompt("number 1")
-let b = +prompt("number 2")
+// let a = +prompt("number 1")
+// let b = +prompt("number 2")
 
-for (let i=a; i<=b; i++) {if((i%2)===0) {console.log(i)}}
+// for (let i=a; i<=b; i++) {if((i%2)===0) {console.log(i)}}
+// let a = 0;
+// alert( Boolean(a) ); // false
 
- 
+// let age = prompt('Возраст?', 18);
+
+// let message = (age < 3) ? 'Здравствуй, малыш!' :
+//   (age < 18) ? 'Привет!' :
+//   (age < 100) ? 'Здравствуйте!' :
+//   'Какой необычный возраст!';
+
+// alert( message );
+
+// let hour = 12;
+// let minute = 30;
+
+// if (hour == 12 && minute == 30) {
+//   alert( 'The time is 12:30' );
+// }
+// let count = 10
+
+// while (count) {
+//   console.log(count)
+//   count--
+// }
+// let i = 0;
+// while (i < 3) { // выводит 0, затем 1, затем 2
+//   alert( i );
+//   i++;}
+// for (let i = 0; i < 3; i++) {
+
+//     for (let j = 0; j < 3; j++) {
+
+//       let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+//       // Что если мы захотим перейти к Готово (ниже) прямо отсюда?
+
+//     }
+//   }
+
+//   alert('Готово!');
+
+// DZ 18/05
+
+// Пользователь вводит три числа. Если все числа больше 10 и первые два числа делятся на 3, то вывести yes, иначе no
+// let a = +prompt ('enter the first number');
+// let b = +prompt ('enter the number two');
+// let c = +prompt ('enter the third number');
+// if (a>10 && a % 3 === 0);
+// if (b>10 && b % 3 === 0);
+// if (c>10)  {alert ('yes')
+// }
+// else alert ("no")
+
+/*Дано число. Если оно от 2 до 5 включительно, то увеличить его на 10. Если оно от 7 до 40, то уменьшить на 100. 
+Если оно не более 0 или более 3000, то увеличить в 3 раза (то есть умножить на 3)*/
+
+// let x = + prompt ("enter the number");
+// if (x>1 && x<=5) {
+// alert (x+10)
+// }
+// if (x>6 && x<=40) {
+//     alert (x-100)
+// }
+// if (x<0 || x>=3001) {
+//     alert (x*3)
+// }
+// else alert ("no")
+
+//Найти произведение 5⋅6⋅7⋅...⋅111.
+// let number = 1
+// for (let n=5 - начальное значение счетчика; n<=8 условия до которого выполняется цикл; n++ - изминение счетчика) {
+/*  number=number*n; что делает цикл пока не дойдет до условия n<=8}
+    1 цикл 1=1*5 получим 5 ушло в переменную number
+    2 цикл переменная number уже будет равна 5 и цикл начнет считать
+   следуйщий number, котрый будет равняться 5* начальное значение счетчика к нему прибавляется шаг n++
+   nember = 5*6 получим 30, потом 30 будет умножено на второй цикл уже 6 плюс шаг n++
+   тоесть 30*7 и тд}*/
+
+// zadaha на умножение
+
+// let number = 1
+// for (let n=5; n<=111; n++) {
+//     number=number*n;
+// }
+// console.log(number)
+
+// // стороки 4 звезды и 7 звезд НЕ ПОНЯЛ КАК ДЕЛАТЬ 
+// let p = 0;
+// let str = "";
+// while (p < 7) {
+//   str += "*******";
+//   console.log(str);
+//   p++;
+//   let p1 = 0;
+//   while (p1 < 1) {
+//     str = "****";
+//     p1++;
+//     console.log(str);
+//   }
+// }
+
+//   console.log(str1);
+
+// сумма квадратов натуральных чисел
+
+// let number = 0;
+// for (let n=1; n<=10; n++) {
+//     number=number+n**2;
+// }
+// console.log(number)
