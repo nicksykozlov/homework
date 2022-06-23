354b2457a06344d96f480a38b44cbc1a8da79593
nicksykozlov <nicksy.kozlov@gmail.com>
2022-06-23 10:00:08 +0300

    div

--- dom20.06.2022/js2.js
+++ dom20.06.2022/js2.js
@@ -1,11 +1,31 @@
 // Закрасьте все div у которых нет дочерних элементов синим цветом
-const pred = document.body.querySelector('div');
-pred.forEach((div) => {
-    if(k.children.length === 0){
-        k.style.backgroundColor = 'blue';
-    }
-});
-console.log(pred);
+// const first=document.body.querySelector('div');
+// first.parentElement.style.backgroundColor = 'grey'
+// first.node.firstChild.backgroundColor = 'red'
+// if (parg.hasChildNodes()) {
+//     // Таким образом, сначала мы проверяем, не пуст ли объект, есть ли у него дети
+//     var children = parg.childNodes;
 
+//     for (var i = 0; i < children.length; ++i) {
+// сделать что-то с каждым внутренним элементом через children[i]
+// ПРИМЕЧАНИЕ: Список является ссылкой, Добавление или удаление дочерних элементов изменит список
+//     }
+//   }
+// // console.log(first);
 // Закрасьте все div у которых только 1 дочерний элемент желтым
 // Закрасьте все div у которых больше 2 дочерних элементов серым
+
+const divElement = document.body.querySelectorAll("div");
+divElement.forEach ((div) => {
+  if (div.children.length === 0) {
+    div.style.background = "blue";
+  }
+  if (div.children.length ===1) {
+    div.style.background = "yellow";
+  }
+  if (div.children.length === 2) {
+    div.style.background = "grey";
+  }
+});
+
+
