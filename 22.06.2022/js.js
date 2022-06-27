a96cefe0c10ee7e2622bc2f692032819c5f61b63
snitolya <maslop@bk.ru>
2022-06-27 21:13:01 +0300

    27.06

--- 22.06.2022/js.js
+++ 22.06.2022/js.js
@@ -152,7 +152,74 @@
 //     table.border = `3px solid`
 //     document.body.prepend(table)
 //     table.append(tr)
-    
+
 //   }
 // }
 // creatTable(10,5);
+
+
+// 27/06/2022
+// const button = document.querySelector('button')
+// const ul = document.querySelector('ul')
+// let n = 1;
+// button.addEventListener("click", create);
+
+// function create() {
+//     const li = document.createElement('li');
+//     li.textContent = n;
+//     n++;
+//     ul.append(li);
+// };
+// ul.addEventListener('contextmenu', (e) => {
+//     k.preventDefault()
+
+//     if(e.target.tagName == "LI"){
+//         e.target.remove();
+
+//     }
+//     console.log(e.target);
+// });
+
+// function creatTable(n, k) {
+//     let table = document.createElement('table');
+//     for (let i = 1; i < k; i++) {
+//         let tr = document.createElement('tr');
+//         for (let j = 1; j < n; j++) {
+//             let td = document.createElement('td');
+//             td.textContent = i + "-" + j;
+//             tr.prepend(td)
+//         }
+//         table.border = `3px solid`
+//         document.body.prepend(table)
+//         table.append(tr)
+//     }
+// }
+// creatTable(10, 5);
+
+
+// const table = document.querySelector('table');
+
+// table.addEventListener('click', (e) => {
+//     if (e.target.tagName == "TD") {
+//         const tr = e.target.closest('tr');
+       
+//         Array.from(tr.children).forEach(td => {
+//             if(td !== e.target){
+//                 td.style.backgroundColor = 'red'; 
+//             }
+//         });
+//     }
+//    });
+
+// const div = document.querySelector("div");
+// div.addEventListener('mouseover', (e) => {
+// let currentPosition = div.style.left ? div.style.left : 0;
+// currentPosition =   Number.parseInt(currentPosition); 
+// div.style.left = currentPosition + 20 + "px";
+
+// });
+// const div = document.querySelector("div");
+// document.body.addEventListener('click', (e) => {
+//     div.style.left = e.clientX + "px";
+//     div.style.top = e.clientY + "px";
+// });
