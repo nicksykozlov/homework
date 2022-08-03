3cdf57d635309b11d3054aa2caa7360bad2d0abd
snitolya <maslop@bk.ru>
2022-08-03 21:05:07 +0300

    data

---
+++ data/js.js
@@ -0,0 +1,37 @@
+// let start = Date.now();
+// let myBirth = new Date('March 23, 1982 03:24:00').getTime();
+// let finish = start - myBirth;
+// let days = finish / (1000*60*60*24);
+
+// let starost = start + (1000*60*60*24)*12000
+// let itog = starost/(1000*60*60*24)
+// какая дата будет через 2000 дней после рождения
+// let time = myBirth + (1000 * 60 * 60 * 24) * 20000;
+// let day = new Date(time);
+// console.log(time);
+// console.log(day);
+
+
+
+// console.log(start);
+
+// console.log(myBirth);
+// console.log(finish);
+// console.log(days);
+// console.log(starost);
+// console.log(itog);
+// console.log(starost.getMonth());
+
+// какой день недели был 01.01.0001
+// let myBirth = new Date(1,0,1);
+// let b = myBirth.setFullYear(1);
+// b = toLocaleDateString('Ru-ru', {weekday: 'long'});
+// console.log(b); //должен поучиться понедельник
+
+// сколько осталось док окнца дня js
+let data = Date.now();
+let end = new Date('August 03, 2021 00:00:00').getTime()
+let finish = (data - end) / (1000*60*60);
+console.log(data);
+console.log(end);
+console.log(finish);
