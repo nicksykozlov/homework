da6648361193194d7bbc8fbb79a5e01f19113208
snitolya <maslop@bk.ru>
2022-09-26 20:52:42 +0300

    magazine

---
+++ magazine/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import React,{useEffect, useState} from 'react'
+
+function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    (async()=>{
+        const response = await fetch('https://dummyjson.com/products');
+        const json = await response.json()
+        setProducts(json.products); 
+    })()
+},[])
+
+  return (
+    <div className="App">
+      <h1>Выберите продукт</h1>
+            <ul>
+                {products.map(products => <li
+                key={products.id}>
+                    {products.title}
+                    <img src={products.thumbnail} />
+                    <button >Select Product</button>
+                </li>)}
+            </ul>
+    
+    </div>
+  );
+}
+
+export default App;
