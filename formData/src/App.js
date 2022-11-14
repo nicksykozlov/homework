26d09969862e9ea03a3a54f6ef4bc695f95cef1e
nicksykozlov <nicksy.kozlov@gmail.com>
2022-11-14 18:07:50 +0300

    formdata

--- formData/src/App.js
+++ formData/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import Todo from './features/counter/todo';
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (<>
-  
-  </>
-    
+  const [selectedFile, setSelectedFile] = useState();
+
+  const handleChange = (event) => {
+    console.log(event.target.files);
+    setSelectedFile(event.target.files[0]);
+  };
+  return (
+    <>
+      <form id="user-form">
+        <input
+          type="file"
+          onChange={handleChange}
+          //multiple//что бы было можно несколько файлов сразу загружать
+          //accept='images/*., png., img.,' при помощи accept только нужный формат файлов
+          name="img"
+        />
+
+        <input type="submit" value="upload" />
+        <img src="" alt="" />
+        {selectedFile && (
+          <ul>
+            <li>Name:{selectedFile.name}</li>
+            <li>Type:{selectedFile.size}</li>
+            <li>lastModifiedDate :
+            {selectedFile.lastModifiedDate.toLocaleDateString()}</li>
+          </ul>
+        )}
+      </form>
+    </>
   );
 }
 
